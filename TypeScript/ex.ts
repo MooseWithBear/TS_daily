acb6997e6a438db4a812d85bc8dbb824711699c0
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-11-02 12:24:33 +0900

    [init]  typescript init

---
+++ TypeScript/ex.ts
@@ -0,0 +1,46 @@
+// // type Member = [number, boolean];
+// // type 이름 = [string, string]
+// // let a:[string,number]=['홍',250];
+
+
+
+// // a= ['k',1];
+// // // a= [1,'k'];
+// // console.log('a: ', a);
+
+// // let 김:이름 = ['신','성'];
+// // console.log('김: ', 김);
+
+
+// // a[0].toUpperCase();
+// // // a[1].toUpperCase();
+
+// // let kim:Member = [123,true];
+// // kim = [100, false];
+// // // kim = ['홍길동', 100];
+
+
+// console.log('--------------------');
+
+
+// function sayHello():void {
+//     console.log('helloooooo');
+// }
+
+// function sayHello2():number {
+//     return 100; 
+// }
+
+// function sayHello3(a:number, b:number):number {
+//     return a+b;
+// }
+
+// sayHello();
+// console.log(sayHello2());
+// console.log(sayHello3(100,200));
+
+
+// const isGreater = (a: number, b: number): boolean => {
+// 	return a > b;
+// }
+// console.log(isGreater(100,200));
