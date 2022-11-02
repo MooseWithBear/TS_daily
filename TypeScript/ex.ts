56860d3c68b61c650150babb9097a928ce0378e0
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-11-02 13:21:47 +0900

    [study] 타입스크립트 기초

--- TypeScript/ex.ts
+++ TypeScript/ex.ts
@@ -44,3 +44,124 @@
 // 	return a > b;
 // }
 // console.log(isGreater(100,200));
+
+
+// // let user:object{};
+// console.log('---------------------');
+// // let user:{name:string, age:number}//안에 객체 또한 타입을 지정해주어야 한다.
+
+// console.log('---------------------');
+
+// type Member = {
+// 	name:string,
+// 	age:number
+// }
+// let user:Member
+// console.log('---------------------');
+
+// user = {
+// 	name:'홍길동',
+// 	age: 30
+// }
+
+// console.log(user.name, user.age);
+
+
+
+
+// interface User {
+// 	name: string;
+// 	age: number;
+// }
+
+// let user : User = {
+// 	name:'홍길동',
+// 	age:30
+// } 
+// user.age=10;
+// console.log(user.name, user.age)
+
+
+
+
+// interface User {
+// 	name:string;
+// 	age:number;
+// 	gender? : string; //필수값이었지만 ?를 넣음으로써 있어도 없어도 되는 값으로 변한다.
+// }
+
+// let user : User = {
+// 	name:'홍길동',
+// 	age : 30,
+// }
+
+// user.age=10;
+// user.gender = 'male';	
+// console.log(user.name, user.age)
+// console.log(user.name, user.age, user.gender)
+
+
+
+// interface User {
+// 	name:string;
+// 	age:number;
+// 	gender?:string;
+// 	readonly birthYear : number;
+// }
+
+// let user : User = {
+// 	name : '홍길동',
+// 	age:30,
+// 	birthYear:2000
+// }
+
+// user.age=10;
+// user.gender = 'male';
+// user.birthYear = 2022;
+// console.log(user.name, user.age);
+
+
+
+// type Score = 'A' | 'B' | 'C' | 'F';
+
+// interface User {
+// 	name:string;
+// 	age:number;
+// 	gender?:string;
+// 	readonly birthYear :number;
+// 	[key:number] : Score;
+// }
+
+// let user : User = {
+// 	name:'홍길동',
+// 	age : 30,
+// 	birthYear :2000,
+// 	1:'A',
+// 	2:'B',
+// 	3:'C',
+// 	4:'K'
+// }
+
+// user.age=10;
+// user.gender = 'male';
+// //user.birthYear = 2022; //readonly 이기때문에 에러발생
+// console.log(user.name , user.age); 
+
+
+interface Add {
+	(num1:number, num2:number): number;
+	// (x:number, y:number): number; //도 된다
+}
+const add :Add =function(x,y) {
+	return x+y;
+}
+console.log(add(10,20));
+
+interface IsAdult {
+	(age:number):boolean;
+}
+
+const kim: IsAdult = (age) => {
+	return age >19;
+}
+console.log(kim(30));
